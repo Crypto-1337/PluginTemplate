e51fb0b8c545cde0967839ae189845ec18fa6482
Collin <collin@lona-development.org>
2024-09-24 14:57:40 +0200

    Update PluginTemplate

--- src/Plugin.php
+++ src/Plugin.php
@@ -1,13 +1,23 @@
 <?php
-
+//Namespace from plugin.json
 namespace Plugin;
-
+//Import PluginBase
 use LonaDB\Plugins\PluginBase;
-
+//Class name from plugin.json
 class MainClass extends PluginBase
 {
+    //The onEnable funciton is always run once the plugin has been loaded
+    //You should use this as your constructor
     public function onEnable() : void
     {
-        $this->GetLogger()->Load($this->GetName() . " has been enabled");
+        $this->GetLogger()->Load($this->GetName() . " on version " . $this->GetVersion() . " has been enabled");
+    }
+
+    //You can create your own event handling:
+    public function onTableCreate(string $executor, string $name) : void {
+        $this->GetLogger()->Plugin($this->GetName(), "A table has been created: " . $name)
+    }
+    public function onTableDelete(string $executor, string $name) : void {
+        $this->GetLogger()->Plugin($this->GetName(), "A table has been deleted: " . $name)
     }
 }
